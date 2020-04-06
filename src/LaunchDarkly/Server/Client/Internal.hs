3ccf13db0ea8bf516775c291d5f28fd3cf7c2fc7
LaunchDarklyCI <dev@launchdarkly.com>
2020-04-06 21:49:41 +0000

    Releasing version 1.0.4

--- src/LaunchDarkly/Server/Client/Internal.hs
+++ src/LaunchDarkly/Server/Client/Internal.hs
@@ -25,7 +25,7 @@ newtype Client = Client ClientI
 
 -- | The version string for this library.
 clientVersion :: Text
-clientVersion = "1.0.3"
+clientVersion = "1.0.4"
 
 -- | The status of the client initialization.
 data Status
