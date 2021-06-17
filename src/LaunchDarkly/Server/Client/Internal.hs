fb5cf94349f0b595879cba792fab6d64bd412fdc
LaunchDarklyCI <dev@launchdarkly.com>
2021-06-17 22:46:34 +0000

    executing delegated release

--- src/LaunchDarkly/Server/Client/Internal.hs
+++ src/LaunchDarkly/Server/Client/Internal.hs
@@ -25,7 +25,7 @@ newtype Client = Client ClientI
 
 -- | The version string for this library.
 clientVersion :: Text
-clientVersion = "2.1.1"
+clientVersion = "2.2.0"
 
 -- | The status of the client initialization.
 data Status
