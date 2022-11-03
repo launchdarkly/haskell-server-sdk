db9922c053baac580dab945d5cbcb299859a44db
Matthew Keeler <mkeeler@launchdarkly.com>
2022-11-03 11:01:14 -0400

    Update version in generated cabal file as part of release

--- .ldrelease/update-version.sh
+++ .ldrelease/update-version.sh
@@ -7,6 +7,11 @@ TEMP_FILE=${TARGET_FILE}.tmp
 sed "s/version:             .*/version:             ${LD_RELEASE_VERSION}/" "${TARGET_FILE}" > "${TEMP_FILE}"
 mv "${TEMP_FILE}" "${TARGET_FILE}"
 
+TARGET_FILE=launchdarkly-server-sdk.cabal
+TEMP_FILE=${TARGET_FILE}.tmp
+sed "s/^\(version:\s\+\).*/\1${LD_RELEASE_VERSION}/" "${TARGET_FILE}" > "${TEMP_FILE}"
+mv "${TEMP_FILE}" "${TARGET_FILE}"
+
 TARGET_FILE=src/LaunchDarkly/Server/Client/Internal.hs
 TEMP_FILE=${TARGET_FILE}.tmp
 sed "s/clientVersion = \".*\"/clientVersion = \"${LD_RELEASE_VERSION}\"/" "${TARGET_FILE}" > "${TEMP_FILE}"
