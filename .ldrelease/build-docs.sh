440d5aac5adb89d2bca9251d5931e8c4cbc51bc4
Matthew M. Keeler <mkeeler@launchdarkly.com>
2023-12-20 12:28:46 -0500

    ci: Replace releaser with release please (#65)

--- .ldrelease/build-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# This only runs in the Linux build, since the docs are the same for all platforms.
-stack --no-terminal haddock
-
-cp -a "$(stack --no-terminal path --dist-dir)"/doc/html/launchdarkly-server-sdk/* "${LD_RELEASE_DOCS_DIR}"
--- .ldrelease/prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# The SDK's pcre-light package requires these external dependencies
-apt-get update && apt-get install -y libpcre3-dev pkg-config
-
-wget -c https://github.com/phlummox/hup/releases/download/v0.3.0.3/hup-0.3.0.3-linux-amd64.tar.xz -O - | tar -xJ -C /usr/local/bin
-
-stack --no-terminal setup
